e6e31d23001592c884a3d92aff566a966c9c3bc4
ErionHeqimi <121134554+ErionHeqimi@users.noreply.github.com>
2024-02-26 00:53:56 +0100

    Arrow And Head

--- src/App.js
+++ src/App.js
@@ -7,39 +7,30 @@ const LabelWithDescription = () => {
 
   const toggleDescription = () => {
     setIsDescriptionVisible(!isDescriptionVisible);
-    setIsThemeClicked(!isThemeClicked); 
+    setIsThemeClicked(!isThemeClicked);
+    // Toggle the up/down arrow class
+    const shenja = document.getElementById('shenja');
+    shenja.classList.toggle('up');
   };
 
   return (
-  <div className="container">
+    <div className="container">
       {/* Label */}
       <div onClick={toggleDescription} className="label1">
         <strong>Why park a domain name in Parkname?</strong>
-        <span id="shenja">&#11167;</span>
-        
-       
+        <div id="shenja" className="down"></div>
       </div>
-    
+      
       {/* ALL Description */}
       {isDescriptionVisible && (
         <div className={`description-container ${isThemeClicked ? 'theme-clicked' : ''}`}>
-          {/* Label  on top of description */}
-          <div onClick={toggleDescription} className="label2">
-            <strong>Why park a domain name in Parkname?</strong>
-            
-            <span id="shenja">&#11165;</span>
-          </div>
-
           {/* Description */}
           <div className="description">
             <p>Parkname is the leading industry standard for domain name parking and monetization services. We offer a wide variety of services to help you achieve success.</p>
           </div>
         </div>
-    
       )}
-    
-  </div>
-    
+    </div>
   );
 };
 
