3de2e77d6926d9a6b38ec7cbbfd3571b974e9bfb
ErionHeqimi <121134554+ErionHeqimi@users.noreply.github.com>
2024-02-25 23:52:19 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './LabelWithDescription.css';
 
-function App() {
+const LabelWithDescription = () => {
+  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+  const [isThemeClicked, setIsThemeClicked] = useState(false);
+
+  const toggleDescription = () => {
+    setIsDescriptionVisible(!isDescriptionVisible);
+    setIsThemeClicked(!isThemeClicked); 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <div className="container">
+      {/* Label */}
+      <div onClick={toggleDescription} className="label1">
+        <strong>Why park a domain name in Parkname?</strong>
+        <span id="shenja">&#11167;</span>
+        
+       
+      </div>
+    
+      {/* ALL Description */}
+      {isDescriptionVisible && (
+        <div className={`description-container ${isThemeClicked ? 'theme-clicked' : ''}`}>
+          {/* Label  on top of description */}
+          <div onClick={toggleDescription} className="label2">
+            <strong>Why park a domain name in Parkname?</strong>
+            
+            <span id="shenja">&#11165;</span>
+          </div>
 
-export default App;
+          {/* Description */}
+          <div className="description">
+            <p>Parkname is the leading industry standard for domain name parking and monetization services. We offer a wide variety of services to help you achieve success.</p>
+          </div>
+        </div>
+    
+      )}
+    
+  </div>
+    
+  );
+};
+
+export default LabelWithDescription;
